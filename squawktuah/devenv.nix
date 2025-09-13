17127a3b5060ce0566e7271ed983ecf9be1a97d7
cakeforcat <cakeforcat@gmail.com>
2025-09-14 00:38:00 +0100

    init squawtuah

---
+++ squawktuah/devenv.nix
@@ -0,0 +1,30 @@
+{
+  pkgs,
+  lib,
+  ...
+}:
+let
+  buildInputs = with pkgs; [
+    stdenv.cc.cc
+    libuv
+    zlib
+  ];
+in
+{
+  env = {
+    LD_LIBRARY_PATH = "${lib.makeLibraryPath buildInputs}";
+  };
+  
+  languages.python = {
+    version = "3.13";
+    enable = true;
+    uv = {
+      enable = true;
+      sync.enable = true;
+    };
+  };
+
+  enterShell = ''
+    . .devenv/state/venv/bin/activate
+  '';
+}
